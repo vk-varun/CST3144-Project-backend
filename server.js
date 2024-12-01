cca8a1961854d8eafda9b4c3a6e5b33fb80fed8f
vk-varun <97547208+vk-varun@users.noreply.github.com>
2024-12-01 14:32:19 -0700

    Add CORS headers

--- server.js
+++ server.js
@@ -11,6 +11,15 @@ app.use((req, res, next) => {
     next();
 })
 
+// CORS headers to allow cross-origin requests 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");  
+    next();  
+})
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
